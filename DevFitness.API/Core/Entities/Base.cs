62690f04928e6f3330422f0eac4699ff77347c2e
gunn3r71 <lucasgunner3@gmail.com>
2021-06-04 20:04:53 -0300

    Adicionando entidades de dominio

---
+++ DevFitness.API/Core/Entities/Base.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Core.Entities
+{
+    public abstract class Base
+    {
+        protected Base()
+        {
+            this.CreatedAt = DateTime.Now;
+            this.Active = true;
+        }
+
+        public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; private set; }
+
+
+        public void Desactivate()
+        {
+            this.Active = false;
+        }
+    }
+}
---
+++ DevFitness.API/Core/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Core.Entities
+{
+    public class User : Base
+    {
+        public User(string fullName, decimal height, decimal weight, DateTime birthDate) : base()
+        {
+            FullName = fullName;
+            Height = height;
+            Weight = weight;
+            BirthDate = birthDate;
+        }
+
+        public string FullName { get; private set; }
+        public decimal Height { get; private set; }
+        public decimal Weight { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        public IEnumerable<Meal> Meals { get; private set; }
+    }
+}
