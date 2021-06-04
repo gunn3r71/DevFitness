21a148a2e461fc65ef9acd4d66999b95b857a0c2
gunn3r71 <lucasgunner3@gmail.com>
2021-06-04 20:35:35 -0300

    Criando controladores, input models e view models da API

---
+++ DevFitness.API/Controllers/MealsController.cs
@@ -0,0 +1,50 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    [Route("api/v1/users/{userId}/[controller]")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int userId)
+        {
+            //return NotFound();
+            return Ok();
+        }
+        
+        [HttpGet("{mealId}")]
+        public IActionResult Get(int userId,int mealId)
+        {
+            //return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(int userId,[FromBody] CreateMealInputModel meal)
+        {
+            return CreatedAtAction(nameof(Get), new { Id = 10 }, meal);
+        }
+
+        [HttpPut("{mealId}")]
+        public IActionResult Put(int userId, int mealId, [FromBody] UpdateMealInputModel meal)
+        {
+            //return NotFound();
+            //return BadRequest();
+            return NoContent();
+        }
+
+        [HttpDelete("{mealId}")]
+        public IActionResult Delete(int userId, int mealId)
+        {
+            return NoContent();
+        }
+    }
+}
+}
---
+++ DevFitness.API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            //return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel user)
+        {
+            return CreatedAtAction(nameof(Get), new { Id = 10}, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] UpdateUserInputModel user)
+        {
+            //return NotFound();
+            //return BadRequest();
+            return NoContent();
+        }
+    }
+}
