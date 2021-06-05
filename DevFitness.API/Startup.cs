64a5c31d6afe5df0b5c023beb3edeb3653835b54
gunn3r71 <lucasgunner3@gmail.com>
2021-06-05 00:13:52 -0300

    Dados de acesso ao banco

--- DevFitness.API/Startup.cs
+++ DevFitness.API/Startup.cs
@@ -1,16 +1,11 @@
+using DevFitness.API.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevFitness.API
 {
@@ -26,6 +21,13 @@ namespace DevFitness.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Configurando bd
+            string ConnectionString = Configuration.GetConnectionString("DevFitnessCs");
+
+            services.AddDbContext<DevFitnessDbContext>(
+                options => {
+                    options.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+                });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
